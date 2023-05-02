5fe95924880e9a14dd5f4dab09e6b2f540dea797
Jhon Moofarry <jhon.moofarry00@gmail.com>
2023-05-01 23:56:45 -0500

    iniciando herosApp

--- 06-heroesApp/src/app/heroes/heroes.module.ts
+++ 06-heroesApp/src/app/heroes/heroes.module.ts
@@ -7,7 +7,8 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
-
+import { MaterialModule } from '../material/material.module';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
@@ -15,11 +16,9 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
     ListPageComponent,
     NewPageComponent,
     SearchPageComponent,
-    LayoutPageComponent
+    LayoutPageComponent,
+    CardComponent,
   ],
-  imports: [
-    CommonModule,
-    HeroesRoutingModule
-  ]
+  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
-export class HeroesModule { }
+export class HeroesModule {}
