b7158b9ff34e54d8609335e970083c2e3ad1719b
Jhon Moofarry <jhon.moofarry00@gmail.com>
2023-05-13 00:40:11 -0500

    Guards

--- 06-heroesApp/src/app/app-routing.module.ts
+++ 06-heroesApp/src/app/app-routing.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
+import { AuthGuard } from './auth/guards/auth.guard';
+import { PublicGuard } from './auth/guards/public.guard';
+
 // dominio.com/
 const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ),
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [PublicGuard],
+    canMatch: [PublicGuard],
   },
   {
     path: 'heroes',
-    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule ),
+    loadChildren: () =>
+      import('./heroes/heroes.module').then((m) => m.HeroesModule),
+    canActivate: [AuthGuard],
+    canMatch: [AuthGuard],
   },
   {
     path: '404',
@@ -19,16 +27,16 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'heroes',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: '**',
     redirectTo: '404',
-  }
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- 06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -7,37 +7,47 @@ import { environments } from '../../../environments/environments';
 
 @Injectable({ providedIn: 'root' })
 export class HeroesService {
+
   private baseUrl: string = environments.baseUrl;
 
-  constructor(private http: HttpClient) {}
 
-  getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  constructor(private http: HttpClient) { }
+
+
+  getHeroes():Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
   }
 
-  getHeroById(id: string): Observable<Hero | undefined> {
-    return this.http
-      .get<Hero>(`${this.baseUrl}/heroes/${id}`)
-      .pipe(catchError((error) => of(undefined)));
+  getHeroById( id: string ): Observable<Hero|undefined> {
+    return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        catchError( error => of(undefined) )
+      );
   }
 
-  getSuggestions(query: string): Observable<Hero[]> {
-    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  getSuggestions( query: string ): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
   }
 
-  addHero(hero: Hero): Observable<Hero> {
-    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+
+  addHero( hero: Hero ): Observable<Hero> {
+    return this.http.post<Hero>(`${ this.baseUrl }/heroes`, hero );
   }
 
-  updateHero(hero: Hero): Observable<Hero> {
-    if (!hero.id) throw Error('Hero id is required');
-    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  updateHero( hero: Hero ): Observable<Hero> {
+    if ( !hero.id ) throw Error('Hero id is required');
+
+    return this.http.patch<Hero>(`${ this.baseUrl }/heroes/${ hero.id }`, hero );
   }
 
-  deleteHeroById(id: string): Observable<boolean> {
-    return this.http.delete(`${this.baseUrl}/heroes/${id}`).pipe(
-      catchError((error) => of(false)),
-      map((resp) => true)
-    );
+  deleteHeroById( id: string ): Observable<boolean> {
+
+    return this.http.delete(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        map( resp => true ),
+        catchError( err => of(false) ),
+      );
   }
+
+
 }
